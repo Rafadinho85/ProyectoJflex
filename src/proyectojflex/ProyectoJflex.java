6063fce3c25b68766056c35b8154c03b8e20052e
Jery Ramírez <rafa85jogo@gmail.com>
2019-02-12 22:03:06 -0600

    Proyecto Generador Lexico

---
+++ src/proyectojflex/ProyectoJflex.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectojflex;
+
+/**
+ *
+ * @author Asus
+ */
+public class ProyectoJflex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
